7e4bb98e1ef132b524c08cc3c4c55546ca774f49
Artem <90412503+artemmoroz0v@users.noreply.github.com>
2023-04-01 17:54:20 +0300

    Add files via upload

---
+++ MAI-Functional-Programming/lab2/Data.fsx
@@ -0,0 +1,51 @@
+// Part 2
+#load "One.fsx"
+open One
+
+let constNewLine = "\n"
+
+//Task 1. Find average mark for each subject
+printfn "%s" "Task 1. Average grade for each subject:"
+let avgGradeForSubject (subjectAbbr: string) =
+    Data.marks
+    |> List.filter (fun (_, subj, _) -> subj = subjectAbbr)
+    |> List.averageBy (fun (_, _, grade: float) -> grade)
+for subjectAbbr, subjectName in Data.subjs do
+    let avgGrade: float = avgGradeForSubject subjectAbbr
+    printfn "Average grade for subject %s (%s) is: %.2f" subjectName subjectAbbr avgGrade
+printfn "%s" constNewLine
+
+
+//Task 2. Print a list of students who failed the session for each group
+printfn "%s" "Task 2. Print a list of students who failed the session for each group:"
+let failedStudents group =
+    Data.marks
+    |> Seq.filter (fun (_, _, grade) -> grade <= 2)
+    |> Seq.filter (fun (surname, _, _) -> Data.studs |> Seq.exists (fun (s, g) -> s = surname && g = group))
+    |> Seq.map (fun (surname, subject, _) -> (surname, subject))
+    |> Seq.groupBy (fun (_, subject) -> subject)
+    |> Seq.map (fun (subject, students) -> (subject, students |> Seq.map fst |> Seq.toList))
+    |> List.ofSeq
+for group: int in Data.studs |> List.map snd |> List.distinct do
+    printfn "Students who failed the session in group № %d are:" group
+    let failedStudents = failedStudents group
+    for (_, students) in failedStudents do
+        printfn "%A" students
+printfn "%s" constNewLine
+
+
+//Task 3. Find average grade for each student
+printfn "%s" "Task 3. Average grade for each student:"
+let gradesByStudent =
+    Data.marks |> List.groupBy (fun (name, _, _) -> name)
+let averageGradeForStudent grades =
+    grades |> List.averageBy (fun (_, _, grade) -> grade)
+for (name, _) in Data.studs do
+    let studentGrades =
+        gradesByStudent
+        |> List.tryFind (fun (n, _) -> n = name)
+        |> Option.map (fun (_, grades) -> grades)
+        |> Option.defaultValue []
+    let average = averageGradeForStudent studentGrades
+    printfn "Average grade for student %s is: %f" name average
+printfn "%s" constNewLine
---
+++ MAI-Functional-Programming/lab2/Solution.fsx
@@ -0,0 +1,39 @@
+// Part 1
+let list = [11; 22; 33; 44; 55; 66; 77; 88; 99; 100]
+
+
+// Method 1: Library Function 
+let libraryFunction list = 
+    let min = list |> Seq.min
+    let max = list |> Seq.max
+    min * max
+printfn "Method 1: Library Function\t%d\n" (libraryFunction list)
+
+
+// Method 2: Recursion
+let rec Recursion (list: int list) =
+    let rec auxilliaryFunction (min:int) (max:int) = function
+        | [] -> min * max
+        | x::xs -> 
+            let newMin = if x < min then x else min
+            let newMax = if x > max then x else max
+            auxilliaryFunction newMin newMax xs
+    match list with
+    | [] -> failwith "List is empty"
+    | x::xs -> auxilliaryFunction x x xs
+printfn "Method 2: Recursion\t%d\n" (Recursion list)
+
+
+// Method 3: Tail recursion
+let rec tailRecursion list =
+    match list with
+    | [] -> failwith "The list is empty!"
+    | [x] -> (x, x)
+    | head::tail -> 
+        let (min, max) = tailRecursion tail
+        if head < min then (head, max)
+        elif head > max then (min, head)
+        else (min, max)
+let (min, max) = tailRecursion list
+let result = min * max
+printfn "Method 3: Tail Recursion\t%d\n" (result)
